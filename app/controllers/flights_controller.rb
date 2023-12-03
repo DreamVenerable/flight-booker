627a6eda5933289b30ae79398fbd84477ee88e7c
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-12-03 21:26:57 +0200

    flight views working

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,21 @@
 class FlightsController < ApplicationController
+
   def index
     @airport_options = Airport.all.map { |c| [c.airport_code] }
     @flight_date = Flight.all.map { |d| [d.trip_datetime.strftime("%Y/%m/%d")] }.uniq
+    @flights = found_flights
+  end
 
-    unless params
-      @flights = Flight.joins(:dep_airport, :arr_airport).where('dep_airport.airport_code' => params[:dep_airport], 'arr_airport.airport_code' => params[:arr_airport], trip_datetime: params[:datetime].to_datetime.all_day)
+  private
+
+  def found_flights
+    unless params['datetime'].nil?
+
+      Flight.joins(:dep_airport, :arr_airport)
+      .where('dep_airport.airport_code' => params['dep_airport'],
+             'arr_airport.airport_code' => params['arr_airport'],
+             'trip_datetime' => params['datetime'].to_datetime.all_day)
+
     end
   end
 end
